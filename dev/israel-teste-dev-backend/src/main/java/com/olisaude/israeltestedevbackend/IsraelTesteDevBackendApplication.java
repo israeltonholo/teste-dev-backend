57f7237608d0dde4ce56b36e244a0780962c92c5
Israel <israeltonholo@hotmail.com>
2022-01-30 09:50:58 -0300

    adding adding docker-compose

---
+++ dev/israel-teste-dev-backend/src/main/java/com/olisaude/israeltestedevbackend/IsraelTesteDevBackendApplication.java
@@ -0,0 +1,13 @@
+package com.olisaude.israeltestedevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsraelTesteDevBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsraelTesteDevBackendApplication.class, args);
+	}
+
+}
---
+++ dev/israel-teste-dev-backend/src/test/java/com/olisaude/israeltestedevbackend/IsraelTesteDevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olisaude.israeltestedevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsraelTesteDevBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
